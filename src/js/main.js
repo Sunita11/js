3c73bd486b0707f243e7019b218d2fc2e3092ec4
Sunita <sunita2210sharma@gmail.com>
2017-12-12 17:47:46 +0530

    adding preset-env

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 import React,{Component} from 'react'
 import {render} from 'react-dom'
 import {AddInput} from '../../src/export-module/AddFile'
-import {AddForm} from '../../src/export-module/AddFormFile'
 
 
 render (
-	<AddInput />, 
+	<AddInput/>, 
 	document.getElementById('addFile')
 )
